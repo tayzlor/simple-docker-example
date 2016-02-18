739e100661df6b1beec8f3d1d8973470a0f9cbf9
Graham Taylor <gtayzlor@gmail.com>
2016-02-18 17:46:09 +0000

    v2.0.0

--- src/server.js
+++ src/server.js
@@ -10,10 +10,10 @@ http.createServer(function(req, res) {
   res.write(
     '<!doctype html>\n<html lang="en">\n' +
     '\n<meta charset="utf-8">\n<title>Node.js simple web server</title>\n' +
-    '<style type="text/css">body {width: 35em;margin: 0 auto;font-family: Tahoma, Verdana, Arial, sans-serif;}</style>\n' +
+    '<style type="text/css">body {background: #2196F3;width: 35em;margin: 0 auto;font-family: Tahoma, Verdana, Arial, sans-serif;}</style>\n' +
     '\n\n<h1>Hello!</h1>\n' +
     '<div id="content">\n' +
-    '<p>Application version <strong>1.0.0</strong></p>' +
+    '<p>Application version <strong>2.0.0</strong></p>' +
     '<p>If you see this page, then you are looking at a web server running in a Docker container. Here are some details about the container</p>' +
     '\n\n<p>Application ID <strong>' + appId + '</strong></p>' +
     '\n\n<p>Container ID <strong>' + hostname + '</strong></p>' +
